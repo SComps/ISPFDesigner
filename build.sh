e487a34a05e88c848f15a1cea342e77aa80d8e2f
ScottJ <westdalefarmer@gmail.com>
2025-12-14 06:54:53 -0500

    Remove apt-get dependency check from build script

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "Installing dependencies (clang, zlib1g-dev)..."
-sudo apt-get update && sudo apt-get install -y clang zlib1g-dev
 
 echo "Building single-file executable for Linux..."
 dotnet publish -c Release --self-contained true /p:PublishSingleFile=true -o Publish
