c944297c841c3432d85db61b443d53d1e721296c
ScottJ <westdalefarmer@gmail.com>
2025-12-21 09:51:10 -0500

    Remove extra CR (0x0D) from export line endings

--- PanelWriter.vb
+++ PanelWriter.vb
@@ -5,12 +5,12 @@ Namespace ISPFDesigner
         Public Shared Sub WriteToFile(filename As String, buffer(,) As Char, rows As Integer, cols As Integer, attrManager As AttributeManager, fieldProperties As Dictionary(Of (Integer, Integer), FieldProperty))
             Dim allBytes As New List(Of Byte)()
             Dim encoding As System.Text.Encoding = System.Text.Encoding.ASCII
-            Dim crlf As Byte() = {13, 10}
+            Dim lf As Byte() = {10} ' Use only LF to avoid "extra 0D" issues on some transfer tools
 
             ' Helper to add a line to the byte list
             Dim AddLine As Action(Of String) = Sub(text)
                 allBytes.AddRange(encoding.GetBytes(text))
-                allBytes.AddRange(crlf)
+                allBytes.AddRange(lf)
             End Sub
 
             ' 1. Write Attributes
